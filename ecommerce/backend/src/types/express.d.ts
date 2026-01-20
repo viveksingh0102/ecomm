dd33f1ad8280465a59d68f4b40e36fffe842cacd
viveksingh0102 <92083263+viveksingh0102@users.noreply.github.com>
2026-01-20 18:35:19 +0530

    Delete ecommerce/backend directory

--- ecommerce/backend/src/types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from "../models/user.model"
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: IUser
-    }
-  }
-}
--- ecommerce/backend/src/utils/token.ts
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken"
-
-export const generateToken = (userId: string): string => {
-    return jwt.sign(
-        { id: userId },
-        process.env.JWT_SECRET as string,
-        { expiresIn: "7d" }
-    )
-}
